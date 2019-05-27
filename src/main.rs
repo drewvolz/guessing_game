7b6e80262704ab660f748c7b879ba5cc2e0f39c4
Drew Volz <volz.drew@gmail.com>
2019-05-26 18:57:35 -0700

    Formatting and checking if the guess has changed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut prev_guess = None;
 
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -20,15 +20,19 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+        if Some(guess) == prev_guess {
+            println!("Doubling down on that last one?");
+        } else {
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
             }
+            prev_guess = Some(guess);
         }
+        println!();
     }
 }
